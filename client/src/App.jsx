4c282ccd0af454ef9bea6aaed1996025716d1be2
ezolbest <ezramamo13@gmail.com>
2023-07-15 19:59:24 -0800

    configuring

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h2>hello world</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
